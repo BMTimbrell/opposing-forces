f921a6107cc03948a2f25e01aa24c899d8ca3928
Ben <bmtimbrell@gmail.com>
2023-12-20 20:35:30 +0000

    added death animation for enemies

--- classes/Enemy.js
+++ classes/Enemy.js
@@ -8,16 +8,23 @@ export default class Enemy {
         this.positionX = positionX;
         this.positionY = positionY;
         this.markedForDeletion = false;
-        this.lives = 1;
         this.image = document.getElementById('ships');
+        this.frameX = 64;
+        this.frameY = 17;
+        this.animationDelay = 3;
+        this.animationTimer = this.animationDelay;
+        this.animationFrameX = 72;
+        this.animationFrameY = 48;
+        this.maxAnimationFrame = this.animationFrameX + 8 * 4;
+        this.lives = 1;
     }
     
     draw(context) {
         /*context.strokeRect(this.x, this.y, this.width, this.height);*/
         context.drawImage(
             this.image, 
-            64, 
-            17, 
+            this.frameX, 
+            this.frameY, 
             this.width / this.game.scale, 
             this.height / this.game.scale,
             this.x,
@@ -32,16 +39,29 @@ export default class Enemy {
         this.y = y + this.positionY;
 
         // check collision with projectiles
-        this.game.projectilesPool.forEach(projectile => {
-            if (!projectile.free && this.game.checkCollision(projectile, this)) {
-                this.hit(1);
-                projectile.reset();
-            }
-        });
-
+        if (this.lives > 0) {
+            this.game.projectilesPool.forEach(projectile => {
+                if (!projectile.free && this.game.checkCollision(projectile, this)) {
+                    this.hit(1);
+                    projectile.reset();
+                }
+            });
+        }
+        
         if (this.lives < 1) {
-            this.markedForDeletion = true;
-            if (!this.game.gameOver) this.game.score++;
+            this.image = document.getElementById('animations');
+            this.frameX = this.animationFrameX
+            this.frameY = this.animationFrameY;
+            this.animationTimer--;
+            if (this.animationTimer === 0) {
+                this.animationFrameX += 8;
+                this.animationTimer = this.animationDelay;
+            }
+            if (this.frameX > this.maxAnimationFrame) { 
+                this.markedForDeletion = true;
+            }
+
+            if (!this.game.gameOver && this.markedForDeletion) this.game.score++;
         }
 
         // check collision with player
@@ -61,5 +81,12 @@ export default class Enemy {
 
     hit(damage) {
         this.lives -= damage;
+    }
+}
+
+export class strongEnemy extends Enemy {
+    constructor(game, positionX, positionY) {
+        super(game, positionX, positionY);
+        this.lives = 3;
     }
 }
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import Game from './Game.js'
+import Game from './classes/Game.js'
 
 window.addEventListener('load', () => {
     const canvas = document.getElementById('canvas');
